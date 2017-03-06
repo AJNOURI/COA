d9325c005a6689403b608c6353cdda3c9260f05b
AJ NOURI <AJNOURI@users.noreply.github.com>
2017-03-06 09:29:50 +0100

    Create osrdo-setup.sh

---
+++ misc/osrdo-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set appropriate hostname & IP address
+echo "192.168.0.108 rdo-newton rdo-newton.localdomain" >> /etc/hosts
+hostnamectl set-hostname rdo-newton
+
+# Prepare the host
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+systemctl enable network
+systemctl start network
+
+# Install repository
+yum install -y centos-release-openstack-newton
+yum install -y https://rdoproject.org/repos/rdo-release.rpm
+yum update -y
+
+# Install openstack
+yum install -y openstack-packstack
+# Change interface name facing extenal network according to your needs.
+packstack --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ovs-bridge-interfaces=br-ex:eno16777736 --os-neutron-ml2-type-drivers=vxlan,flat
