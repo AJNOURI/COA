ec513551aa0e3f990489bb8c400f82b0065167cc
AJ NOURI <AJNOURI@users.noreply.github.com>
2017-08-05 02:20:42 +0200

    Update oneliner_log_inspection.sh

--- misc/oneliner_log_inspection.sh
+++ misc/oneliner_log_inspection.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # rapid CLI oneliner log inspection inside all log files
 # Lookig for keywords "fail", "error", "unable", "warning".
 
-for i in $(ls /var/log/*/*.log); do echo "=========="; echo $i; echo "========="; tail $i| egrep -i warning; done
+for i in $(ls /var/log/*/*.log); do echo "=========="; echo $i; echo "========="; tail $i| egrep -i $1; done
 
 
 
